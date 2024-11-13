ecf36d92c7d27b6340d8a6792b45b1670928d36e
Yuriy-Chernov <82463564+Yuriy-Chernov@users.noreply.github.com>
2024-11-14 01:32:38 +0300

    Add files via upload

---
+++ lesson-01/01.js
@@ -0,0 +1,13 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum = 0;
+let targetNum = 20
+while(targetNum > 0){
+    sum = sum + targetNum;
+
+    targetNum--
+}
+
+console.log(sum)
