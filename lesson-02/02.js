54766064899afb4fb8bbb7bd86d062cb13406f2e
Yuriy-Chernov <82463564+Yuriy-Chernov@users.noreply.github.com>
2024-11-17 02:38:05 +0300

    Update 02.js

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -10,7 +10,7 @@
 let dice1 = Math.floor(Math.random() * 6) + 1;
 let dice2 = Math.floor(Math.random() * 6) + 1;
 let isWinningDouble;
-if (dice1 = dice2 && dice1 > 3){isWinningDouble = true }
+if (dice1 == dice2 && dice1 > 3){isWinningDouble = true }
 
 
 console.log('Первый бросок: ' + dice1)
