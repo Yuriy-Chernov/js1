8f0aab0f617ec2696bcfda0c0272263e18f71db2
Yuriy-Chernov <82463564+Yuriy-Chernov@users.noreply.github.com>
2024-11-17 03:42:42 +0300

    Update 03.js

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -8,8 +8,8 @@ const score = 70 // тестовое значение, можно изменят
 let grade
 if (score > 0 && score <= 49 ){grade = 'F'}
 else if (score <= 69){grade = 'D' }
-else if (score <= 79){grader = "C"}
-else if (score <= 89){grader = "B"}
-else if (score <= 100){grader = "A"}
+else if (score <= 79){grade = "C"}
+else if (score <= 89){grade = "B"}
+else if (score <= 100){grade = "A"}
 
 
